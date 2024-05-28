757cc8c565f3975d8948ccac54b7d05738b1be08
keetrap <parteekkamboj112@gmail.com>
2024-05-29 01:48:43 +0530

    Package Build

--- setup.py
+++ setup.py
@@ -1,28 +1,62 @@
-from setuptools import setup, find_packages
+import io
+import os
+from pathlib import Path
 
+from setuptools import find_packages, setup
+
+
+# Metadata of package
+NAME = 'prediction_model'
+DESCRIPTION = 'Loan Prediction Model'
+URL = 'https://github.com/keetrap/Packaging_ml_model'
+EMAIL = 'parteekkamboj112@gmail.com'
+AUTHOR = 'Nachiketh Murthy'
+REQUIRES_PYTHON = '>=3.7.0'
+
+pwd = os.path.abspath(os.path.dirname(__file__))
+
+# Get the list of packages to be installed
+def list_reqs(fname='requirements.txt'):
+    with io.open(os.path.join(pwd, fname), encoding='utf-8') as f:
+        return f.read().splitlines()
+
+try:
+    with io.open(os.path.join(pwd, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+
+# Load the package's __version__.py module as a dictionary.
+ROOT_DIR = Path(__file__).resolve().parent
+PACKAGE_DIR = ROOT_DIR / NAME
+about = {}
+with open(PACKAGE_DIR / 'VERSION') as f:
+    _version = f.read().strip()
+    about['__version__'] = _version
 
 setup(
-    name="prediction_model",
-    version="1.0.0",
-    author="Prateek Kamboj",
-    author_email="parteekkamboj112@gmail.com",
-    description="A prediction model package",
-    long_description_content_type="text/markdown",
-    url="https://github.com/keetrap/Packaging_ml_models",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    package_data={'prediction_model': ['VERSION']},
+    # install_requires
+    extras_require={},
     include_package_data=True,
-    install_requires=[
-        "numpy",
-        "pandas",
-        "scikit-learn",
-        "joblib"
-
-    ],
+    license='MIT',
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
     ],
-    python_requires='>=3.7',
 )
