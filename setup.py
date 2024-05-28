53068212293943a872bd9107f31de1b26920fe24
keetrap <parteekkamboj112@gmail.com>
2024-05-29 02:03:15 +0530

    final

--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ NAME = 'prediction_model'
 DESCRIPTION = 'Loan Prediction Model'
 URL = 'https://github.com/keetrap/Packaging_ml_model'
 EMAIL = 'parteekkamboj112@gmail.com'
-AUTHOR = 'Nachiketh Murthy'
+AUTHOR = 'Prateek Kamboj'
 REQUIRES_PYTHON = '>=3.7.0'
 
 pwd = os.path.abspath(os.path.dirname(__file__))
 
 # Get the list of packages to be installed
-def list_reqs(fname='requirements.txt'):
-    with io.open(os.path.join(pwd, fname), encoding='utf-8') as f:
-        return f.read().splitlines()
+# def list_reqs(fname='requirements.txt'):
+#     with io.open(os.path.join(pwd, fname), encoding='utf-8') as f:
+#         return f.read().splitlines()
 
 try:
     with io.open(os.path.join(pwd, 'README.md'), encoding='utf-8') as f:
@@ -47,7 +47,25 @@ setup(
     url=URL,
     packages=find_packages(exclude=('tests',)),
     package_data={'prediction_model': ['VERSION']},
-    # install_requires
+    install_requires=[
+        'colorama==0.4.6',
+        'iniconfig==2.0.0',
+        'joblib==1.4.2',
+        'numpy==1.26.4',
+        'packaging==24.0',
+        'pandas==2.2.2',
+        'pluggy==1.5.0',
+        'pytest==8.2.1',
+        'python-dateutil==2.9.0.post0',
+        'pytz==2024.1',
+        'scikit-learn==1.5.0',
+        'scipy==1.13.1',
+        'six==1.16.0',
+        'threadpoolctl==3.5.0',
+        'tzdata==2024.1',
+        'setuptools==67.8.0',
+        'wheel==0.38.4',
+    ],
     extras_require={},
     include_package_data=True,
     license='MIT',
